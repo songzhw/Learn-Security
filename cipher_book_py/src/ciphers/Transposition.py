7765a4eef677a829e18daf3a22995e93a850acbf
songzhw <songzhw2012@gmail.com>
2018-05-04 10:45:40 -0400

    transposition cipher

--- cipher_book_py/src/ciphers/Transposition.py
+++ cipher_book_py/src/ciphers/Transposition.py
@@ -1,3 +1,6 @@
+import math
+
+
 def encrypt(key, msg):
     table = [''] * key  # => 若key为3, 那结果就是['', '', '']
     idx = 0
@@ -7,18 +10,39 @@ def encrypt(key, msg):
         idx += 1  # python中没有idx++这样的操作
         idxInTable = idx % key
 
-    print("==============")
-    for c in table:
-        print(c)
-    print("==============")
     return ''.join(table)
 
 
-if __name__ == '__main__':
-    plainText = "common sense"
-    encrypted = encrypt(3, plainText)
-    print("encrypted =", encrypted)
+def decrypt(key, msg):
+    length = len(msg)
+    blanStartPos = length % key  # blankCount = key - (length%key)
+    rowCount = math.ceil(length / key)
+    ret = [''] * rowCount  # 其实key就是columnCount
 
+    row = 0
+    col = 0
+    for char in msg:
+        isLast1 = row == rowCount
+        isLast2 = row == (rowCount - 1) and col >= blanStartPos
+
+        if isLast1 or isLast2:
+            row = 0
+            col += 1
+
+        ret[row] += char
+        row += 1
+
+    return ''.join(ret)
+    # Python中没有&&, ||, 只有and, or. 可读性好些
+
+
+if __name__ == '__main__':
+    plainText = "common senses"
+    key = 3
+    encrypted = encrypt(key, plainText)
+    decrypted = decrypt(key, encrypted)
+    print("encrypted =", encrypted)
+    print("decrypted = " + decrypted)
 
     # s1 = [''] * 4
     # print("value = ", s1)  #=> ['', '', '', '']
@@ -27,18 +51,23 @@ if __name__ == '__main__':
     #
     # for c in "what a day":
     #     print(c)  #=> 每一个char一行
+    #
+    # print("ceil = ", math.ceil(10 / 3)) #=> 4
+    #
+    # 格式化输出 (%d 数字, %s字符串, %r布尔)
+    # print("col = %d, row = %d, char = %s, %r, %r" % (col, row, char, isLast1, isLast2))
 
 '''
-plain text = common sense
+plain text = common senses, key = 3
 
 加密后的table是:
 ==============
-cm n
+cm ns
 ooss
 mnee
 ==============
 
-encrpyted = cm noossmnee
+encrpyted = cm nsoossmnee
 
 
 '''
