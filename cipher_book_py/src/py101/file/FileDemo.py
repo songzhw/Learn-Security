926697a08bfa9d2e72bfad3524ba6ad20f27e7db
songzhw <songzhw2012@gmail.com>
2018-05-05 23:16:37 -0400

    py101 file

--- cipher_book_py/src/py101/file/FileDemo.py
+++ cipher_book_py/src/py101/file/FileDemo.py
@@ -1,7 +1,12 @@
-file = open('a.txt', 'r')  # "a.txt"等同于"./a.txt"
-content = file.read()
-print(content)
-file.close() # python会在程序结束时自动关闭所有打开的文件. 这是自己手动关.
+import os
+
+isAtxtExist = os.path.exists('a.txt')
+print(isAtxtExist) #=> True
+if(isAtxtExist):
+    file = open('a.txt', 'r')  # "a.txt"等同于"./a.txt"
+    content = file.read()
+    print(content)
+    file.close() # python会在程序结束时自动关闭所有打开的文件. 这是自己手动关.
 
 f2 = open('b.txt', 'w') # 没b.txt文件时, "w"模式会自动创建文件
 f2.write("line1\nline2")
