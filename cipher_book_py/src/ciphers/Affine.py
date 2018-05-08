b29fe4ee30cb6e06b4a86e3c420fbfdd80d8d8a0
songzhw <songzhw2012@gmail.com>
2018-05-08 07:57:41 -0400

    affine cipher

--- cipher_book_py/src/ciphers/Affine.py
+++ cipher_book_py/src/ciphers/Affine.py
@@ -16,22 +16,35 @@ def getRandomKey():
 def encrypt(key1, key2, msg):
     ret = ''
     for char in msg:
-        pos = SYMBOLS.find(char)
-        newPos = (pos * key1 + key2) % size
-        ret += SYMBOLS[newPos]
+        if char in SYMBOLS:
+            pos = SYMBOLS.find(char)
+            newPos = (pos * key1 + key2) % size
+            ret += SYMBOLS[newPos]
+        else :
+            ret += char
     return ret
 
-
+def decrypt(key1, key2, msg):
+    key1D = mi(key1, size)
+    ret = ''
+    for char in msg:
+        if char in SYMBOLS:
+            pos = SYMBOLS.find(char)
+            newPos = (pos - key2) * key1D % size
+            ret += SYMBOLS[newPos]
+        else:
+            ret += char
+    return ret
 
 if __name__ == '__main__':
-    plainText = "Yes, Prime Minister"
+    plainText = "Yes, Prime Minister书籍"
     key1, key2 = getRandomKey()
     print("key1, key2 = ", key1, key2)
 
     encrypted = encrypt(key1, key2, plainText)
     print("encrypted = ", encrypted)
-
-
+    decrypted = decrypt(key1, key2, encrypted)
+    print("decrypted = ", decrypted)
 
 # 若只想传一个key, 那就可以传 key1 * size + key2, 即KEY
 # 解析时就用key1 = KEY // size, key2 = KEY % size, 就解析出来了
