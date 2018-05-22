084ef003af954b9e0e8a441c7202f0c2ccf19423
songzhw <songzhw2012@gmail.com>
2018-05-22 02:15:25 -0400

    rsa

--- cipher_book_py/src/ciphers/rsa/RsaCipher.py
+++ cipher_book_py/src/ciphers/rsa/RsaCipher.py
@@ -46,9 +46,41 @@ def encrypt(keyFileName, plainText, resultFileName, blockSize = DEFAULT_BLOCK_SI
         sys.exit("error: block size need to >= key size")
 
     encryptedBlocks = encryptBlocks(plainText, (n, e), blockSize)
-    return ""
+    for i in range(len(encryptedBlocks)):
+        encryptedBlocks[i] = str(encryptedBlocks[i])
+    encrypted = ",".join(encryptedBlocks)
+    return (encrypted, len(plainText), blockSize)
+
+def decrypt(msg, keyFileName):
+    keySize, n, d = readKeyFile(keyFileName)
+
+    # RSA要求blockSize >= keySize
+    if keySize < blockSize * 8:
+        sys.exit("error: block size need to >= key size")
+
+    blocks = []
+    for block  in msg.split(','):
+        blockInt = int(block)
+        blocks.append(pow(blockInt, d, n))
+
+    ret = []
+    for blockInt in blocks:
+        submsg = []
+        for i in range(blockSize - 1, -1, -1):
+            if len(ret) + i < len(msg):
+                asciiNumber = blockInt // (BYTE_SIZE ** i)
+                blockInt = blockInt % (BYTE_SIZE ** i)
+                submsg.insert(0, chr(asciiNumber))
+        ret.extend(submsg)
+    return ''.join(ret)
+
+
 
 
 if __name__ == '__main__':
     plainText = "WorkManager is designed for a task that is guarateened to execute"
-    encrypt("rsa01_public.txt", plainText, "rsa01_encrypted.txt")
+    encrypted, leng, blockSize = encrypt("rsa01_public.txt", plainText, "rsa01_encrypted.txt")
+
+    decrypted = decrypt(encrypted, "rsa01_private.txt")
+    print("encrypted = ", encrypted)
+    print("decrypted = ", decrypted)
